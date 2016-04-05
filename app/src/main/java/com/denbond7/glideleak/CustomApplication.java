ca410d0debbc0a8f38bd79fd1c01240bb269bb68
Robert Papp <papp.robert.s@gmail.com>
2016-04-05 13:08:43 +0200

    Rename folders and packages, delete unused files

---
+++ app/src/main/java/com/denbond7/glideleak/CustomApplication.java
@@ -0,0 +1,19 @@
+package com.denbond7.glideleak;
+
+import android.app.Application;
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * @author Denis Bondarenko
+ *         Date: 09.03.2016
+ *         Time: 11:15
+ *         E-mail: DenBond7@gmail.com
+ */
+public class CustomApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    LeakCanary.install(this);
+  }
+}
